52afa39db13a06e08bf6218024599f1c98bb59f5
RONY SULCA <RONY SULCA>
2017-07-01 16:57:08 -0700

    Creating Package

---
+++ R/geom_hurricane.R
@@ -0,0 +1,10 @@
+# Geom Hurricane!
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+geom_hurrican <- function() {
+}
